787929043491959f827eece7de01aa550b2bfff3
Berna Büyükada <52409956+BernaBuykada@users.noreply.github.com>
2019-07-31 16:13:36 +0300

    for dongusu

---
+++ for_dongusu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+<select>
+    <?php 
+    for ($i=1; $i <= 15 ; $i++) {?>
+    <option value="<?php echo $i ?>"><?php echo $i ?></option>
+    <?php }
+    ?>
+    }
+</body>
+</html>
+
