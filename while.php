acd236e2a0704281685642f09f2013ed321cefb7
Berna Büyükada <52409956+BernaBuykada@users.noreply.github.com>
2019-08-01 12:01:18 +0300

    while

---
+++ while.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+//while döngüsü
+
+$say=0;
+
+while ($say <= 20) { 
+	if ($say % 2 == 0) {
+		echo "$say 2'ye tam bölünür <br>";
+	}
+$say++;
+}
+echo "Döngü tamamlandı";
+?>
+
+</body>
+</html>
+
